8ddb1fdae5a4309e6509cef968b5a941a1505c9b
Atticz22 <attilacz22@gmail.com>
2022-10-25 11:43:30 +0300

    New Project created.

---
+++ ProjectC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectC 
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
